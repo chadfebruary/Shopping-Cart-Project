1bcf9264a4cf1b9e1497754bfcb28fa92221ddbd
Chad February <chadfebruary@gmail.com>
2016-10-10 04:40:09 +0200

    Updated inventory table

--- Shopping Cart Project/RoasteryCoffees.php
+++ Shopping Cart Project/RoasteryCoffees.php
@@ -42,60 +42,34 @@
 
 <p>Welcome message goes here </p>
 <div id="shoppingCart">
-<div class=="txt-heading">Shopping Cart<a id="btnEmpty" href="RoasteryCoffees.php?action=empty">Empty Cart</a></div>
+<div class="txt-heading">Shopping Cart<a id="btnEmpty" href="RoasteryCoffees.php?action=empty">Empty Cart</a></div>
 
 <?php
 if(isset($_SESSION["cartItem"])){
+	$ItemTotal = 0;
 ?>
 
 <table cellpadding="10" cellspacing="1">
 <tbody>
 <tr>
-<th><strong>Product</strong></th>
+<th><strong>Picture</strong></th>
 <th><strong>Name</strong></th>
 <th><strong>Description</strong></th>
+<th><strong>Weight</strong></th>
 <th><strong>Price</strong></th>
 </tr>
 <?php 
-	foreach($_SESSION["cartItem"] as $Item)
-	{
+	foreach($_SESSION["cartItem"] as $Item){
 		?>
 		<tr>
-		<td><strong><?php echo $Item["Picture"];?></strong></td>
-		<td><?php echo $Item["Name"];?></td>
-		<td><?php echo $Item["Description"];?></td>
-		<td><?php echo $Item["Weight"];?></td>
-		<td><?php echo $Item["Price"];?></td>
+		<td><strong><?php echo $Item["picture"];?></strong></td>
+		<td><?php echo $Item["name"];?></td>
+		<td><?php echo $Item["description"];?></td>
+		<td><?php echo $Item["weight"];?></td>
+		<td><?php echo $Item["price"];?></td>
 		</tr>
 	<?php
 		}
-	?>
-	<?php
-	/*if (count($ErrorMsgs) == 0){
-		$SQLString = "SELECT * FROM inventory WHERE storeID = 'COFFEE'";
-		$QueryResult = $DBConnection->query($SQLString);
-		
-		if($QueryResult === FALSE){
-			$ErrorMsgs[] = "Query was not completed succesfully"." Error code ".$DBConnection->errno.": ".$DBConnection->error;
-		}
-		else{
-			echo "<table width = '100%'>\n";
-			echo "<tr><th>Product</th> <th>Description</th> <th>Price for each</th></tr>\n";
-			
-			while (($Row = $QueryResult->fetch_assoc()) !== NULL){
-				echo "<tr><td>".htmlentities($Row['name'])."</td>\n";
-				echo "<td>".htmlentities($Row['description'])."</td>\n";
-				printf("<td>$%.2f</td></tr>\n", $Row['price']);
-			}
-			
-			echo "</table>";
-	}
-	}
-	
-	if (count($ErrorMsgs)){
-		foreach ($ErrorMsgs as $Msg){
-			echo "<p>".$Msg."</p>";
-		}*/
 	?>
 </tbody>
 </table>
@@ -112,33 +86,22 @@ if(isset($_SESSION["cartItem"])){
 		if(!empty($ProductList)){
 			while($Row = $ProductList->fetch_assoc()){
 		?>
-			<?/*<div class="product-item">
-			<form method="POST" action="RoasteryCoffees?action=add&code=<?php echo $ProductListArray[$Key]["productID"];?>">
-			<div class="product-image"><img src="<?php echo $ProductListArray[$Key]["picture"];?>"></div>
-			<div><strong><?php echo $ProductListArray[$Key]["name"];?></strong></div>
-			<div><strong><?php echo $ProductListArray[$Key]["description"];?></strong></div>
-			<div class="product-price"><?php echo "R".$ProductArray[$Key]["price"];?></div>
-			<div>
-				<input type="text" name="quantity" value="1" size="2"/>
-				<input type ="submit" value="Add to cart" class="btnAddAction"/>
-			</div>
-			</form>
-			</div>*/
-			?>
 			<div class="product-item">
-			<form method="POST" action="RoasteryCoffees?action=add&code=<?php echo $Row["productID"];?>">
-			<div class="product-image"><img src="<?php echo $Key["picture"];?>"></div>
-			<div><strong><?php echo $Key["name"];?></strong></div>
-			<div><strong><?php echo $Key["description"];?></strong></div>
-			<div><strong><?php echo $Key["weight"];?></strong></div>
-			<div class="product-price"><?php echo "R".$Key["price"];?></div>
-			<div>
-				<input type="text" name="quantity" value="1" size="2"/>
-				<input type ="submit" value="Add to cart" class="btnAddAction"/>
+				<form method="POST" action="RoasteryCoffees?action=add&code=<?php echo $Row["productID"];?>">
+					<div class="product-image"><img src="<?php echo $Row["picture"];?>"></div>
+					<div><strong><?php echo $Row["name"];?></strong></div>
+					<div><strong><?php echo $Row["description"];?></strong></div>
+					<div><strong><?php echo $Row["weight"];?></strong></div>
+					<div class="product-price"><?php echo "R".$Row["price"];?></div>
+					<div>
+						<input type="text" name="quantity" value="1" size="2"/>
+						<input type ="submit" value="Add to cart" class="btnAddAction"/>
+					</div>
+				</form>
 			</div>
-			</form>
-			</div>
-	<?php } ?>
+	<?php }
+		}
+	?>
 </div>
 </body>
 </html>
